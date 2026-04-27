055601e6c974388f056368560c24eaf30c810320
m260 <shota.M.020626.S.K.F@gmail.com>
2026-04-27 22:01:49 +0900

    fix bug: installation script

--- logger.sh
+++ logger.sh
@@ -6,6 +6,9 @@ WORKING_DIR="$(dirname "$0")"
 VENV_DIR=".venv"
 
 install() {
+
+	echo "Installing..."
+
 	# exit if python is not installed
 	if !(type "python3" > /dev/null 2>&1); then
 		echo "python3 not found. Install it"
@@ -13,13 +16,18 @@ install() {
 	fi
 	
 	# create virtual env
-	python3 -m venv $working_dir/$VENV_DIR
+	python3 -m venv $WORKING_DIR/$VENV_DIR
 	
 	# install pip packages
 	"$WORKING_DIR/$VENV_DIR/bin/pip" install -r "$WORKING_DIR/requirements.txt"
+
+	echo ""
+	echo "====== Installation successful! ======"
+	echo ""
+	echo ""
 }
 
-if [ -f "$WORKING_DIR/$VENV_DIR/bin/python" ];then
+if [ ! -d "$WORKING_DIR/$VENV_DIR" ];then
 	install
 fi
 
